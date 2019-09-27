ece75759c585dccf3a2f8dd328a9f47df8e565b3
wuguojia <956957153@qq.com>
2019-09-27 15:33:14 +0800

    app.vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import app from './App.vue'
 import { Header} from 'mint-ui'
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name, Header);
 
 var vm=new Vue({
